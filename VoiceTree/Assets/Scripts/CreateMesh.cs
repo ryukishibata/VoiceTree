4cc32fcf5ebc074b260926b1a2c720807cd30c52
ryukishibata <gunjyousky@gmailcom>
2016-09-24 00:17:58 +0900

    ・円柱描画の修正

--- VoiceTree/Assets/Scripts/CreateMesh.cs
+++ VoiceTree/Assets/Scripts/CreateMesh.cs
@@ -4,14 +4,25 @@ using System.Collections;
 public class CreateMesh : MonoBehaviour
 {
 
-    public Vector3 Pivot = new Vector3(0, 0, 0);
+    public Vector3 Pivot;
     public float Height;
     public float TopRadius;
     public float BottomRadius;
     public int Divition;
+    public bool onTopPolygon;
+    public bool onBottomPolygon;
+
 
     private Mesh mesh;
 
+    /*------------------------------------------------------------- cnt_True */
+    int cnt_True(bool a, bool b)
+    {
+        if (a && b) return 2;
+        else if (a || b) return 1;
+        else return 0;
+    }
+    /*=======================================================================*/
     // Use this for initialization
     void Start()
     {
@@ -20,54 +31,79 @@ public class CreateMesh : MonoBehaviour
         mesh = new Mesh();
         Vector3[] newVertices = new Vector3[(this.Divition + 1) * 2];
         Vector2[] newUV = new Vector2[(this.Divition + 1) * 2];
-        int[] newTriangles = new int[(this.Divition * 2) * 3];
-
-        // 上円頂点座標の指定.
-        newVertices[0] = new Vector3(0, 0, 0);
-        for(int i = 1; i <= this.Divition + 1; i++)
-        {
-            newVertices[i] = new Vector3(
-                Mathf.Cos((float)(i - 1) / this.Divition * 2.0f * Mathf.PI) * TopRadius,
-                0,
-                Mathf.Sin((float)(i - 1) / this.Divition * 2.0f * Mathf.PI) * TopRadius
-                );
-        }
-        // 下円頂点座標の指定.
-        for (int i = 0; i < this.Divition + 1; i++)
-        {
-            newVertices[i] = new Vector3(
-                Mathf.Cos((float)i / this.Divition * 2.0f * Mathf.PI) * TopRadius,
-                0.0f,
-                Mathf.Sin((float)i / this.Divition * 2.0f * Mathf.PI) * TopRadius
-                );
-        }
-        newVertices[this.Divition] = new Vector3(0, 0, 0);
+        int num_triangles = ((cnt_True(onTopPolygon, onBottomPolygon) + 2) * 4) * 3;
+        Debug.Log(num_triangles);
+        int[] newTriangles = new int[num_triangles];
 
 
-
-
-        // UVの指定 (頂点数と同じ数を指定すること).
-        //newUV[0] = new Vector2(0.0f, 0.0f);
-        //newUV[1] = new Vector2(0.0f, 1.0f);
-        //newUV[2] = new Vector2(1.0f, 1.0f);
-        //newUV[3] = new Vector2(1.0f, 0.0f);
-        //newUV[4] = new Vector2(0.0f, 0.0f);
-
-
-        /*---------------------- 三角形ごとの頂点インデックスを指定. */
-        // 上円の頂点インデックスを指定
-        for (int i = 0; i < this.Divition; i++){
-            newTriangles[(3*i)+0] = 0;
-            newTriangles[(3*i)+1] = (i + 0) % (this.Divition) + 1;
-            newTriangles[(3*i)+2] = (i + 1) % (this.Divition) + 1;
-        }
-        // 下円の頂点インデックスを指定
+        /*------------------------------------------------ 各頂点座標の指定 */
+        //--- 上円.
         for (int i = 0; i < this.Divition; i++)
         {
-            newTriangles[(3 * i)] = i % this.Divition;
-            newTriangles[(3 * i) + 1] = (i + 1) % this.Divition;
-            newTriangles[(3 * i) + 2] = this.Divition;
+            newVertices[i] = new Vector3(
+                Pivot.x + Mathf.Cos((float)(i) / this.Divition * 2.0f * Mathf.PI) * TopRadius,
+                Pivot.y + Height,
+                Pivot.z + Mathf.Sin((float)(i) / this.Divition * 2.0f * Mathf.PI) * TopRadius
+                );
         }
+        newVertices[this.Divition] = new Vector3(Pivot.x, Pivot.y + Height, Pivot.z);
+
+        //--- 下円.
+        for (int i = this.Divition+1; i < newVertices.Length; i++)
+        {
+            newVertices[i] = new Vector3(
+                Pivot.x + Mathf.Cos((float)(i - (this.Divition + 1)) / this.Divition * 2.0f * Mathf.PI) * BottomRadius,
+                Pivot.y,
+                Pivot.z + Mathf.Sin((float)(i - (this.Divition + 1)) / this.Divition * 2.0f * Mathf.PI) * BottomRadius
+                );
+        }
+        newVertices[newVertices.Length - 1] = new Vector3(Pivot.x, Pivot.y, Pivot.z);
+        Debug.Log(newVertices.Length);
+
+        for (int i = 0; i< newVertices.Length; i++)
+        {
+            Debug.Log(i + ":" + newVertices[i]);
+        }
+
+        /*------------------------------------------------------- 図形の描画 */
+        //--- 側面.
+        for(int i = 0; i < this.Divition; i++)
+        {
+                newTriangles[(6 * i) + 0] = i;
+                newTriangles[(6 * i) + 1] = (i+1) % Divition;
+                newTriangles[(6 * i) + 2] = i + (Divition + 1);
+
+
+                newTriangles[(6 * i) + 3] = (i + 1) % Divition + (Divition + 1);
+                newTriangles[(6 * i) + 4] = i + (Divition + 1);
+                newTriangles[(6 * i) + 5] = (i + 1) % Divition;
+        }
+        ////--- 上円.
+        //if (onTopPolygon)
+        //{
+        //    /*----------------------------------------------------- UVの指定 */
+        //
+        //    /*--------------------------- 三角形ごとの頂点インデックスの指定 */
+        //    for (int i = 0; i < this.Divition; i++)
+        //    {
+        //        newTriangles[(3 * i) + 0] = this.Divition;
+        //        newTriangles[(3 * i) + 1] = (i + 1) % (this.Divition);
+        //        newTriangles[(3 * i) + 2] = (i + 0) % (this.Divition);
+        //    }
+        //}
+        ////--- 下円.
+        //if (onBottomPolygon)
+        //{
+        //    /*----------------------------------------------------- UVの指定 */
+        //
+        //    /*--------------------------- 三角形ごとの頂点インデックスの指定 */
+        //    for (int i = this.Divition; i < this.Divition * 2; i++)
+        //    {
+        //        newTriangles[(3 * i) + 0] = newVertices.Length - 1;
+        //        newTriangles[(3 * i) + 1] = (i + 0) % (this.Divition) + (this.Divition + 1);
+        //        newTriangles[(3 * i) + 2] = (i + 1) % (this.Divition) + (this.Divition + 1);
+        //    }
+        //}
 
 
         mesh.vertices = newVertices;
@@ -80,7 +116,7 @@ public class CreateMesh : MonoBehaviour
         GetComponent<MeshFilter>().sharedMesh = mesh;
         GetComponent<MeshFilter>().sharedMesh.name = "myMesh";
     }
-
+    /*=======================================================================*/
     // Update is called once per frame
     void Update()
     {
